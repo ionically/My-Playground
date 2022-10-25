176df6576e8960832c7ff0b6be68ed421beb297d
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 15:41:10 +0530

    Array completed

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,1 +1,34 @@
+import UIKit
+var item1 = "Apples"
+var item2 = "Oranges"
+
+var shoppinglist = ["Apples", "Oranges", "Bananas"]
+//var shoppinglist = []
+print(shoppinglist[1])
+shoppinglist.count
+shoppinglist.append("Milk")
+shoppinglist
+
+shoppinglist.last
+shoppinglist.first
+shoppinglist.count
+print(shoppinglist)
+shoppinglist.removeAll()
+
+var newArr = [Int]()
+newArr.count
+newArr.append(5)
+newArr.append(6)
+print(newArr)
+newArr[0] = 10
+print(newArr)
+newArr[1] = 20
+print(newArr)
+newArr.append(40)
+newArr.append(60)
+newArr.append(80)
+newArr.append(100)
+print(newArr)
+newArr[5] = 20000
+print(newArr)
 
