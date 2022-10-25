8da64c534d28ecf8e770bd4eec97bf5526211ecb
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 20:14:28 +0530

    12.functions clear all

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,18 +1,21 @@
 import UIKit
-//func are making life much easiers
-
-print("Hello World")
-print("How are You")
 
 func firstFunction() {
     print("Hello World")
     print("How are You")
 }
 
-firstFunction()
-firstFunction()
+func add(num1: Int, num2: Int) {
+    print(num1 + num2)
+}
+
+func multiply(num1: Int, num2: Int) -> Int {
+    return num1 * num2
+}
+print(multiply(num1:5, num2: 3))
+var num = multiply(num1: 10, num2: 2)
+
+print(num)
 
 
-
-
-
+//so how use the func key by using the arrow key also the name of a value and its type
