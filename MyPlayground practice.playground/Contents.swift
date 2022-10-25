88dfc3cba08be51ece9e309d3e1d3c0d00b4749b
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 21:32:41 +0530

    16.inheritance and overriding

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,30 +1,36 @@
-//object oriented programming
-
-//classes an object
-//for example: cards
-//objects are instincts of a class
-//suppose class used as person1 and if its object is Person hence its intinct of this object is answered that is Person
-//so let add properties to class suppose for example we use age with its type and name with its type
-//when we use class we need init funtion so let use this func called init to find class
-//hence there will be a name and a type used with that as shown bellow
-//hence using backslash key for using instead of addingsign(+) or plussign key we try to use backslash (\)
-//here we learn how to use the class with var and mention class as init by storing self data. Class or object with its type and objects as how to use name and age of any person and how to greet each with help of func used with self.name or age
+//inheritance class
+//inheritance: types of cards each case we have diffrent type of class so it can be said to be sub class or we can call it here parentmethod
+// lets use parentmethod func
+//suppose class suv has its own properties so we can use the childmethod for parent class
 import UIKit
-class Person {
-    var name : String
-    var age : Int
-    
-    init(name: String, age: Int) {
-        self.name = name
-        self.age = age
+
+class Parent {
+    func parentmethod() {
+        print("This is a parent method")
     }
-    func greeting() {
-        print("Your name is \(self.name) and you are \(self.age) years old")
-              }
-
+    func getname() {
+        print("This is a parent")
+    }
 }
-var person1 = Person(name:"champ", age: 29)
-person1.age
-person1.name
-person1.greeting()
 
+var p = Parent()
+p.parentmethod()
+
+class Child : Parent {
+    func childmethod() {
+        print("This is a child method")
+    }
+    override func getname() {
+        print("This is a child")
+    }
+}
+
+ var c = Child()
+c.childmethod()
+c.parentmethod()
+c.getname()
+
+//overriding in class
+//overriding methods like not only  unique but general methods such as getname func
+ //   hence it doesn't work unless we inclue overide word in func so just use override while rewrite the same func in child class
+var list = ["clear all"]
