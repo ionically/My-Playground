fec6c84f3d9a93a5697631f945d05c24acd12512
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 22:01:58 +0530

    13.structs

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,36 +1,30 @@
-//inheritance class
-//inheritance: types of cards each case we have diffrent type of class so it can be said to be sub class or we can call it here parentmethod
-// lets use parentmethod func
-//suppose class suv has its own properties so we can use the childmethod for parent class
+//struct is used for grouping together such as example longitude latitudes like locations, types of strings, variable (var), class or objects and its types instead of using the one or more varibles we use struct to use to groiup this variable by using function = struct
 import UIKit
+var name = "champ"
+var latitude = 42
+var longitude = 62
 
-class Parent {
-    func parentmethod() {
-        print("This is a parent method")
-    }
-    func getname() {
-        print("This is a parent")
-    }
+func getloc(name: String, lat: Int, long: Int) {
+    print(name)
 }
+//we can use double in place of int while using decimals for both.
+getloc(name: name, lat: latitude, long: longitude)
 
-var p = Parent()
-p.parentmethod()
-
-class Child : Parent {
-    func childmethod() {
-        print("This is a child method")
-    }
-    override func getname() {
-        print("This is a child")
-    }
+struct userlocation {
+    var name : String
+    var latitude : Int
+    var longitude : Int
 }
+var myloc = userlocation(name: "champ", latitude: 42, longitude: 62)
+myloc.latitude
+myloc.longitude
+myloc.name
 
- var c = Child()
-c.childmethod()
-c.parentmethod()
-c.getname()
+func getloc(loc:userlocation)
+{
+    print(loc.name)
+}
+getloc(loc:myloc)
 
-//overriding in class
-//overriding methods like not only  unique but general methods such as getname func
- //   hence it doesn't work unless we inclue overide word in func so just use override while rewrite the same func in child class
-var list = ["clear all"]
+// hence group in struct by using var then make a location to that in var,  then get your own location by using func in frame of userlocation
+var list = ["Task Completed"]
