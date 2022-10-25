3e0bee8b2a7f039164092bbe5b0cabcc01b7919c
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 19:28:33 +0530

    clear all

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,20 +1,22 @@
 import UIKit
-var age = 20
-for i in 1...10 {
-    for j in 1...8 {
-        if j == 6 {
-            print(i)
-        }
-    }
-}
-for i in 1...10 {
-    for j in 1...8 {
-        if j == 6 {
-            print(j)
-        }
-    }
-}
-//difference
-//i loops 1,2,3,4,5,6,7,8,9,10
-//and here j loops (6666666666) ie; 6 10 times
+//breaks and continue
+//continue is use to skip
+//then break then skip and continue
+//suppose
 
+var age = 15
+for i in 1...10 {
+    if i == 5 && i == 6 {
+        continue
+    }
+    print(i)
+}
+for i in 1...10 {
+    if i == 5 || i == 6 {
+        continue
+    }
+    print(i)
+}
+//(&&)and can be used to for not skipping and continue along with the given integers  but
+//or operator skip both and continue by removing the both skiped integers
+var list = ["end"]
