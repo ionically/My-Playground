b281caa507e88aa349ef44f3fd436cd80bab39e6
ionically <chandnichaudhari.66@gmail.com>
2022-10-25 21:07:20 +0530

    15.class and objects

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,21 +1,30 @@
+//object oriented programming
+
+//classes an object
+//for example: cards
+//objects are instincts of a class
+//suppose class used as person1 and if its object is Person hence its intinct of this object is answered that is Person
+//so let add properties to class suppose for example we use age with its type and name with its type
+//when we use class we need init funtion so let use this func called init to find class
+//hence there will be a name and a type used with that as shown bellow
+//hence using backslash key for using instead of addingsign(+) or plussign key we try to use backslash (\)
+//here we learn how to use the class with var and mention class as init by storing self data. Class or object with its type and objects as how to use name and age of any person and how to greet each with help of func used with self.name or age
 import UIKit
+class Person {
+    var name : String
+    var age : Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    func greeting() {
+        print("Your name is \(self.name) and you are \(self.age) years old")
+              }
 
-func firstFunction() {
-    print("Hello World")
-    print("How are You")
 }
+var person1 = Person(name:"champ", age: 29)
+person1.age
+person1.name
+person1.greeting()
 
-func add(num1: Int, num2: Int) {
-    print(num1 + num2)
-}
-
-func multiply(num1: Int, num2: Int) -> Int {
-    return num1 * num2
-}
-print(multiply(num1:5, num2: 3))
-var num = multiply(num1: 10, num2: 2)
-
-print(num)
-
-
-//so how use the func key by using the arrow key also the name of a value and its type
