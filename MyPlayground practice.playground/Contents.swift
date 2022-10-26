820675707a0da62e6d7a7bf6269ca27db19da6a1
ionically <chandnichaudhari.66@gmail.com>
2022-10-26 15:43:51 +0530

    17.Protocols

--- MyPlayground practice.playground/Contents.swift
+++ MyPlayground practice.playground/Contents.swift
@@ -1,30 +1,26 @@
-//struct is used for grouping together such as example longitude latitudes like locations, types of strings, variable (var), class or objects and its types instead of using the one or more varibles we use struct to use to groiup this variable by using function = struct
 import UIKit
-var name = "champ"
-var latitude = 42
-var longitude = 62
 
-func getloc(name: String, lat: Int, long: Int) {
-    print(name)
+protocol fly {
+    
+    var weight : Int {get set}
+    
+    func fly() -> String
+    
 }
-//we can use double in place of int while using decimals for both.
-getloc(name: name, lat: latitude, long: longitude)
 
-struct userlocation {
-    var name : String
-    var latitude : Int
-    var longitude : Int
+class Pigeon : fly {
+    
+    var weight = 40
+    
+    func fly() -> String {
+        return "Flying"
+    }
+    
 }
-var myloc = userlocation(name: "champ", latitude: 42, longitude: 62)
-myloc.latitude
-myloc.longitude
-myloc.name
+var p = Pigeon()
+print(p.fly())
+p.weight = 20
+print(p.weight)
 
-func getloc(loc:userlocation)
-{
-    print(loc.name)
-}
-getloc(loc:myloc)
-
-// hence group in struct by using var then make a location to that in var,  then get your own location by using func in frame of userlocation
-var list = ["Task Completed"]
+var sent = "\"Finished\""
+print(sent)
